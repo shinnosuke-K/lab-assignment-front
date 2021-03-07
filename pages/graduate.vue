ff2a7997a3f8edd9f7f726ce4b2452fca203db3e
shinnosuke-K <shin.050195@gmail.com>
2021-03-07 14:02:17 +0000

    ログイン後のページの用意

---
+++ pages/graduate.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>graduate</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/lab.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>
+        lab
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
