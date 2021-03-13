7efd07edea9c8f0752be30ec5acd13341d061b4b
shinnosuke-K <shin.050195@gmail.com>
2021-03-13 08:41:02 +0000

    質問項目2の用意

--- pages/graduate.vue
+++ pages/graduate.vue
@@ -1,13 +1,34 @@
 <template>
-    <p>graduate</p>
+    <b-container>
+        <div class="section">
+            <Section :section="section"/>
+        </div>
+    </b-container>
 </template>
 
 <script>
-export default {
+import Section from '@/components/Section'
 
+export default {
+    components: {
+        Section,
+    },
+    data() {
+        // vuexを利用する
+        return {
+            section: {
+                num: 2,
+                show: false,
+            }
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.section {
+    border: solid 1px black;
+    margin: 0% 100px;
+}
 
 </style>
