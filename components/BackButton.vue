8691bd7767cab69b422dcd661be09777f4021293
shinnosuke-K <shin.050195@gmail.com>
2021-03-13 08:37:56 +0000

    進む・戻るボタンの実装

---
+++ components/BackButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button>
+        <img src="@/static/img/outline_chevron_left.png" alt="">
+        戻る
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'BackButton'
+}
+</script>
+
+<style scoped>
+
+button {
+    font-size: 2rem;
+    background-color: #B7B7B7;
+    color: black;
+    border: none;
+    border-radius: 0.5rem;
+    margin: 5% 20% 5% 50%;
+    padding: 15px 10px;
+    min-width: 200px;
+}
+
+</style>
---
+++ components/ProgressButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button>
+        {{ btn.word }}
+        <img src="@/static/img/outline_chevron_right.png" alt="">
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'ProgressButton',
+    props: ['btn']
+}
+</script>
+
+<style scoped>
+button {
+    font-size: 2rem;
+    background-color: #F59944;
+    color: white;
+    border: none;
+    border-radius: 0.5rem;
+    margin: 5% 20% 5% 50%;
+    padding: 15px 10px;
+    min-width: 200px;
+}
+
+</style>
