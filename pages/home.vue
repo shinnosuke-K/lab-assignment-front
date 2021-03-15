82eb590d193f876145dd650925ff0f14cd75892e
shinnosuke-K <shin.050195@gmail.com>
2021-03-15 05:43:14 +0000

    dataに追加

--- pages/home.vue
+++ pages/home.vue
@@ -14,10 +14,52 @@ export default {
         Section,
     },
     data() {
+        // vuexを使う
         return {
             section: [
                 {num: 1, show: false, fixShow: true, route: '/lab'},
                 {num: 2, show: false, fixShow: true, route: 'graduate'},
+            ],
+            laboratories: [
+                  {
+                    name: "応用メディア情報研究室",
+                    menbers: [
+                        {
+                            professor: "大久保",
+                            point: 0,
+                        },
+                        {
+                            professor: "土屋",
+                            point: 0,
+                        },
+                        {
+                            professor: "井本",
+                            point: 0,
+                        }
+                    ]
+                },
+                {
+                    name: "ネットワーク情報システム研究室",
+                    menbers: [
+                        {
+                            professor: "佐藤",
+                            point: 0,
+                        },
+                        {
+                            professor: "小板",
+                            point: 0,
+                        }
+                    ]
+                },
+                  {
+                    name: "知識メカトロ情報システム研究室",
+                    menbers: [
+                        {
+                            professor: "高橋",
+                            point: 0,
+                        },
+                    ]
+                }
             ]
         }
     }
